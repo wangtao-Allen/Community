cec5874965026f575a08e876d8bee40efc4b5eab
wangtao-allen <wangtao_333@icloud.com>
2019-06-23 12:10:47 +0800

    实现持久化登陆状态获取

--- src/main/java/allen/community/mapper/UserMapper.java
+++ src/main/java/allen/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package allen.community.mapper;
 import allen.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by Allen on 2019/06/23
@@ -12,4 +14,7 @@ public interface UserMapper {
 
     @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId}, #{name}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = ${token}")
+    User findByToken(@Param("token") String token);
 }
