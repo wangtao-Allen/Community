fe5bb578f141ad8bba8cab5020daf5efe31be672
wtming01 <wangtao_333@icloud.com>
2019-08-31 13:47:35 +0800

    实现回复功能-添加事务

--- src/main/java/allen/community/service/CommentService.java
+++ src/main/java/allen/community/service/CommentService.java
@@ -10,6 +10,7 @@ import allen.community.model.Comment;
 import allen.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Allen on 2019/08/31
@@ -26,6 +27,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
