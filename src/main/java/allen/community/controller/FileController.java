da8557dfa2af540702d9ea6b6140cd40da57d50d
wtming01 <wangtao_333@icloud.com>
2019-09-08 21:52:29 +0800

    增加markdown 图片上传ucloud功能实现

--- src/main/java/allen/community/controller/FileController.java
+++ src/main/java/allen/community/controller/FileController.java
@@ -1,9 +1,15 @@
 package allen.community.controller;
 
 import allen.community.dto.FileDTO;
+import allen.community.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Allen on 2019/09/08
@@ -11,12 +17,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class FileController {
 
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
     @RequestMapping("file/upload")
     @ResponseBody
-    public FileDTO upload(){
-        FileDTO fileDTO = new FileDTO();
-        fileDTO.setSuccess(1);
-        fileDTO.setUrl("/images/wechat.png");
-        return fileDTO;
+    public FileDTO upload(HttpServletRequest request){
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            String fileName = uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(1);
+            fileDTO.setUrl(fileName);
+            return fileDTO;
+        } catch (Exception e) {
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(0);
+            fileDTO.setMessage("上传失败");
+            return fileDTO;
+        }
     }
 }
