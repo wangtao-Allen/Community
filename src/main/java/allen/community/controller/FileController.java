cb03ddbb315251d23d255a29cfe203e10f8164de
wtming01 <wangtao_333@icloud.com>
2019-09-08 16:59:54 +0800

    增加markdown 图片上传功能实现

---
+++ src/main/java/allen/community/controller/FileController.java
@@ -0,0 +1,22 @@
+package allen.community.controller;
+
+import allen.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Allen on 2019/09/08
+ */
+@Controller
+public class FileController {
+
+    @RequestMapping("file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/wechat.png");
+        return fileDTO;
+    }
+}
---
+++ src/main/java/allen/community/dto/FileDTO.java
@@ -0,0 +1,13 @@
+package allen.community.dto;
+
+import lombok.Data;
+
+/**
+ * Created by Allen on 2019/09/08
+ */
+@Data
+public class FileDTO {
+    private int success;
+    private String message;
+    private String url;
+}
